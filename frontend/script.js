aa6972970d66b38ac3bbdcc815ed72028dc8685b
vignesh7235 <153364566+vignesh7235@users.noreply.github.com>
2026-01-22 18:19:01 +0530

    Create script.js

---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+function login() {
+    alert("Login Successful");
+    window.location.href = "dashboard.html";
+}
+
+function signup() {
+    alert("Signup Successful");
+    window.location.href = "login.html";
+}
